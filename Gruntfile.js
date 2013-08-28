aa15d4938f5a1598b8ea8a61b1e4f690cc4d5bf4
Jason Mulligan <jason.mulligan@avoidwork.com>
2013-08-27 22:08:18 -0400

    Initial commit of code

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function (grunt) {
+	grunt.initConfig({
+		pkg : grunt.file.readJSON("package.json"),
+		concat : {
+			options : {
+				banner : "/**\n" + 
+				         " * <%= pkg.name %>\n" +
+				         " *\n" +
+				         " * @author <%= pkg.author.name %> <<%= pkg.author.email %>>\n" +
+				         " * @copyright <%= grunt.template.today('yyyy') %> <%= pkg.author.name %>\n" +
+				         " * @license <%= pkg.licenses[0].type %> <<%= pkg.licenses[0].url %>>\n" +
+				         " * @link <%= pkg.homepage %>\n" +
+				         " * @module <%= pkg.name %>\n" +
+				         " * @version <%= pkg.version %>\n" +
+				         " */\n"
+			},
+			dist : {
+				src : [
+					"src/intro.js",
+					"src/channel.js",
+					"src/outro.js"
+				],
+				dest : "lib/<%= pkg.name %>.js"
+			}
+		},
+		jshint : {
+			options : {
+				jshintrc : ".jshintrc"
+			},
+			src : "lib/<%= pkg.name %>.js"
+		},
+		nodeunit : {
+			all : ["test/*.js"]
+		},
+		watch : {
+			js : {
+				files : "<%= concat.dist.src %>",
+				tasks : "default"
+			},
+			pkg: {
+				files : "package.json",
+				tasks : "default"
+			}
+		}
+	});
+
+	// tasks
+	grunt.loadNpmTasks("grunt-contrib-concat");
+	grunt.loadNpmTasks("grunt-contrib-nodeunit");
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	// aliases
+	grunt.registerTask("test", [/*"nodeunit",*/ "jshint"]);
+	grunt.registerTask("build", ["concat"]);
+	grunt.registerTask("default", ["build", "test"]);
+};
--- README.md
+++ README.md
@@ -1,4 +1,74 @@
-cqueue
-======
+# cqueue
 
-Channel queue with Promises
+cqueue is a Channel queue for holding a msg/value until it's taken. Promises are used for concurrent operations against the queue.
+
+## How do I use it?
+
+Put something in the queue, and then take it at some point in the future. The queue can only hold 1 value, and the response will indicate what your machine should do, i.e. "continue" or "pause".
+
+```javascript
+var cqueue = require("cqueue"),
+    chan   = cqueue();
+
+chan.take().then(function ( arg ) {
+	if ( arg[1] === null ) {
+		console.log("empty");
+	}
+});
+
+chan.put("hello world");
+
+chan.take().then(function ( arg ) {
+	console.log( arg );
+});
+```
+
+## API
+
+### queue
+#### Property
+
+Array which can hold 1 value
+
+### put
+#### Method
+
+ Puts an item into the Channel
+ 
+ 	method put
+ 	param  {Mixed} arg Item
+ 	return {Array}     Deferred
+
+```javascript
+chan.put(arg).then(function(state) {
+	if (state[0] === "continue") {
+		// arg was added to the channel
+	}
+	else {
+		// arg is not in the channel
+	}
+});
+```
+
+### take
+#### Method
+
+Takes an item from the Channel
+
+	method take
+ 	return {Array} Deferred
+
+```javascript
+chan.take().then(function(state) {
+	if (state[0] === "continue") {
+		// state[1] was taken from the channel
+	}
+	else {
+		// state[1] is null
+	}
+});
+```
+
+## License
+Copyright (c) 2013 Jason Mulligan  
+Licensed under the BSD-3 license.
